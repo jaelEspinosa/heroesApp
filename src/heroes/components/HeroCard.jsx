b74289a6059d968b998c0eba85298b19c03194b8
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2023-01-03 11:51:04 +0100

    sc

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -11,7 +11,7 @@ export const HeroCard = ({
  }) => {
 
 
-    const heroImage = `/heroes/${id}.jpg`
+    const heroImage = `./heroes/${id}.jpg`
   return (
     <div className="col animate__animated animate__fadeIn">
       <div className="card">
